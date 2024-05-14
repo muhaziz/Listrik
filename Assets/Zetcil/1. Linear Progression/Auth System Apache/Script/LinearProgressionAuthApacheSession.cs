0cfa164104dcc928d0996869f05b4eb8bcb6cfb9
muhammad aziz <azizkeren2004@gmail.com>
2024-05-14 18:23:40 +0700

    ya

--- Assets/Zetcil/1. Linear Progression/Auth System Apache/Script/LinearProgressionAuthApacheSession.cs
+++ Assets/Zetcil/1. Linear Progression/Auth System Apache/Script/LinearProgressionAuthApacheSession.cs
@@ -1,16 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Networking;
-using UnityEngine.Events;
+
 public class LinearProgressionAuthApacheSession : MonoBehaviour
 {
     [Header("Auth Settings")]
-    public string CurrentEmail;
-    public InputField CurrentEmailField;
-    public string CurrentNickName;
-    public InputField CurrentNickNameField;
+    public string currentEmail;
+    public InputField currentEmailField;
+    public string currentOrganization;
+    public InputField currentOrganizationField;
+
+    private const string CURRENT_EMAIL_KEY = "CurrentEmail";
+    private const string CURRENT_ORGANIZATION_KEY = "CurrentOrganization";
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +20,12 @@ public class LinearProgressionAuthApacheSession : MonoBehaviour
 
     public void InitializeCurrentSession()
     {
-        CurrentEmail = PlayerPrefs.GetString(LinearProgressionAuthApache.CURRENT_EMAIL);
-        CurrentEmailField.text = CurrentEmail;
+        // Mendapatkan nilai email dan organisasi dari PlayerPrefs
+        currentEmail = PlayerPrefs.GetString(CURRENT_EMAIL_KEY);
+        currentOrganization = PlayerPrefs.GetString(CURRENT_ORGANIZATION_KEY);
+
+        // Menampilkan nilai email dan organisasi di input field
+        currentEmailField.text = currentEmail;
+        currentOrganizationField.text = currentOrganization;
     }
 }
